556e935026c3496b99bda2e1f49e5eb433b09e79
Nick Carchedi <nick.carchedi@gmail.com>
2014-06-12 13:52:10 -0400

    integrate yamlWriter

--- R/yamlWriter.R
+++ R/yamlWriter.R
@@ -1,67 +1,26 @@
-#' DIRECTIONS: 
+#' Create new yaml lesson using yamlWriter
 #' 
-#' 1. Source this file and invoke newYaml(lessonName, courseName), where
-#' lessonName and courseName are strings of your choice such as "My Lesson 1" and 
-#' "My Course". Spaces are allowed in course and lesson names.
-#' This will create a file such as My_Course/My_Lesson_1/lesson.yaml.
-#' The file may appear in the editor automatically. If not, open it manually.
-#' 
-#' 2. Fill in the course meta-information, Author, Organization, etc. as 
-#' indicated in lesson.yaml. 
-#' 
-#' 3. Append instructional units such as multiple-choice questions to 
-#' lesson.yaml by invoking appropriate helper functions. Type hlp() for a 
-#' brief list. Typing qmult(), for instance would append the following 
-#' template to lesson.yaml:
-#' 
-#' - Class: mult_question  
-#'   Output: ask the multiple choice question here
-#'   AnswerChoices: ANS;2;3
-#'   CorrectAnswer: ANS
-#'   AnswerTests: omnitest(correctVal= 'ANS')
-#'   Hint: hint
-#'   
-#' ... which might be manually edited as follows.
-#' 
-#' - Class: mult_question  
-#'   Output: This demonstrates a multiple choice question. Which Scooby-Doo
-#'      character wears an ascot?
-#'   AnswerChoices: Fred Jones;Velma Dinkley;Daphne Blake;Shaggy Rogers
-#'   CorrectAnswer: Fred Jones
-#'   AnswerTests: omnitest(correctVal= 'Fred Jones')
-#'   Hint: This person doesn't say "Jinkies."
-#'   
-#' 4. Save lesson.yaml whenever you append and edit a unit of instruction.
-#' 
-#' 5. Strings may take more than one line provided they are surrounded by 
-#' double quotes. For instance, in the following question the Output: field 
-#' takes up two lines:
-#' 
-#' - Class: text_question
-#'   Output: "This illustrates a question requiring a single phrase text answer. 
-#'      What is the name of the van which carries Scooby's gang?"
-#'   CorrectAnswer: Mystery Machine
-#'   AnswerTests: omnitest(correctVal='Mystery Machine')
-#'   Hint: The gang generally solves a M------? It's the M------ Machine.
-
+#' @param lessonName Name of lesson
+#' @param courseName Name of course
+#' @export
 newYaml <- function(lessonName, courseName){
-  lessonDir <<- file.path(gsub(" ", "_", courseName), 
-                          gsub(" ", "_", lessonName))
+  lessonDir <- file.path(gsub(" ", "_", courseName), 
+                         gsub(" ", "_", lessonName))
   if(file.exists(lessonDir)) {
-    lessonDir <<- normalizePath(lessonDir)
+    lessonDir <- normalizePath(lessonDir)
     stop("Lesson already exists in ", lessonDir)
   }
   dir.create(lessonDir, recursive=TRUE)
-  lessonDir <<- normalizePath(lessonDir)
+  lessonDir <- normalizePath(lessonDir)
   message("Creating new lesson in ", lessonDir)
   writeLines("# Put initialization code in this file.", 
-             file.path(lessonDir,"initLesson.R"))
+             file.path(lessonDir, "initLesson.R"))
   writeLines("# Put custom tests in this file.", 
-             file.path(lessonDir,"customTests.R"))
+             file.path(lessonDir, "customTests.R"))
   # The yaml faq, http://www.yaml.org/faq.html, encourages
   # use of the .yaml (as opposed to .yml) file extension
   # whenever possible.
-  lessonFile <<- file.path(lessonDir, "lesson.yaml")
+  lessonFile <- file.path(lessonDir, "lesson.yaml")
   writeLines(c("- Class: meta", 
                paste("  Course:", courseName),
                paste("  Lesson:", lessonName),
@@ -71,11 +30,16 @@ newYaml <- function(lessonName, courseName){
                paste("  Version:", packageVersion("swirl"))),
              lessonFile)
   file.edit(lessonFile) # May not work due to .yaml extension
+  message("If the lesson file doesn't open automatically, you can open it now to begin editing...")
+  # Set options
+  options(lessonFile = lessonFile)
 }
 
-# yaml writer help
+#' yamlWriter help
+#' 
+#' @export
 hlp <- function(){
-  print("newYaml(course, lesson) -- create a new yaml lesson")
+  print("newYaml(lessonName, courseName) -- create a new yaml lesson")
   print("txt -- just text, no question")
   print("qmult -- multiple choice question")
   print("qcmd -- command line question")
@@ -85,15 +49,20 @@ hlp <- function(){
   print("qtxt -- question requiring a short text answer")
 }
 
-# template for presentation without a question
+#' template for output without a question
+#' 
+#' @export
 txt <- function(){
   lessonFileCheck()
   cat("\n- Class: text
-  Output: put your text output here\n", file=lessonFile, append=TRUE)
+  Output: put your text output here\n", 
+      file=getOption("lessonFile"), append=TRUE)
   invisible()
 }
 
-# template for multiple choice question
+#' Template for multiple choice question
+#' 
+#' @export
 qmult <- function(){
   lessonFileCheck()
   cat("\n- Class: mult_question  
@@ -101,61 +70,97 @@ qmult <- function(){
   AnswerChoices: ANS;2;3
   CorrectAnswer: ANS
   AnswerTests: omnitest(correctVal= 'ANS')
-  Hint: hint\n", file=lessonFile, append=TRUE)
+  Hint: hint\n", 
+      file=getOption("lessonFile"), append=TRUE)
   invisible()
 }
 
+#' Template for R command question
+#' 
+#' @export
 qcmd <- function(){
   lessonFileCheck()
   cat("\n- Class: cmd_question
   Output: explain what the user must do here
   CorrectAnswer: EXPR or VAL
   AnswerTests: omnitest(correctExpr='EXPR', correctVal=VAL)
-  Hint: hint\n", file=lessonFile, append=TRUE)
+  Hint: hint\n", 
+      file=getOption("lessonFile"), append=TRUE)
   invisible()
 }
 
+#' Template for video unit
+#' 
+#' @export
 vid <- function(){
   lessonFileCheck()
   cat("\n- Class: video
   Output: Would you like to watch a short video about ___?
-  VideoLink: 'http://address.of.video'\n", file=lessonFile, append=TRUE)
+  VideoLink: 'http://address.of.video'\n", 
+      file=getOption("lessonFile"), append=TRUE)
   invisible()
 }
 
+#' Template for figure unit
+#' 
+#' @export
 fig <- function(){
   lessonFileCheck()
   cat("\n- Class: figure
   Output: explain the figure here
   Figure: sourcefile.R
-  FigureType: new or add\n", file=lessonFile, append=TRUE)
+  FigureType: new or add\n", 
+      file=getOption("lessonFile"), append=TRUE)
   invisible()
 }
 
+#' Template for exact numerical question
+#' 
+#' @export
 qx <- function(){
   lessonFileCheck()
   cat("\n- Class: exact_question
   Output: explain the question here
   CorrectAnswer: n
   AnswerTests: omnitest(correctVal=n)
-  Hint: hint\n", file=lessonFile, append=TRUE)
-invisible()
+  Hint: hint\n", 
+      file=getOption("lessonFile"), append=TRUE)
+  invisible()
 }
 
+#' Template for text question
+#' 
+#' @export
 qtxt <- function(){
   lessonFileCheck()
   cat("\n- Class: text_question
   Output: explain the question here
   CorrectAnswer: answer
   AnswerTests: omnitest(correctVal='answer')
-  Hint: hint\n", file=lessonFile, append=TRUE)
-invisible()
+  Hint: hint\n", 
+      file=getOption("lessonFile"), append=TRUE)
+  invisible()
 }
 
+#' Set or change lesson you want to work on
+#' 
+#' @export
+setLesson <- function() {
+  options(lessonFile = NULL)
+  lessonFileCheck()
+  message("This lesson is located at ", getOption("lessonFile"))
+  message("\nIf the lesson file doesn't open automatically, you can open it now to begin editing...")
+  invisible()
+}
+
+#' Checks that you are working on a lesson
+#' 
+#' @export
 lessonFileCheck <- function(){
-  while(!exists("lessonFile", globalenv()) || !file.exists(lessonFile)){
+  while(is.null(getOption("lessonFile")) || 
+          !file.exists(getOption("lessonFile"))) {
     course <- gsub(" ", "_", readline("Course name? "))
     lesson <- gsub(" ", "_", readline("Lesson name? "))  
-    lessonFile <<- file.path(course, lesson, "lesson.yaml")
+    options(lessonFile = file.path(course, lesson, "lesson.yaml"))
   }
 }
