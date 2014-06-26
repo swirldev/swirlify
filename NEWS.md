313513bd8bee91d6d2884d868cf8bbee9ebfd84e
Nick Carchedi <nick.carchedi@gmail.com>
2014-06-26 15:58:27 -0400

    add swirl2html

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# swirlify 0.2.1
+
+* Add `swirl2html()` to convert swirl lessons formatted in YAML to R markdown (Rmd) and html files. The output is a stylized webpage that acts as a standalone tutorial based on the original swirl lesson.
+
 # swirlify 0.2
 
 * Add a more bare bones YAML authoring toolset initiated by `new_yaml()`. `hlp()` gives a list of related functions. We plan to integrate this new approach more closely with existing swirlify functions, but it functions well as is.
---
+++ R/swirl2html.R
@@ -0,0 +1,106 @@
+### This only works for swirl lessons formatted in yaml ###
+
+makechunk <- function(item) {
+  paste("```{r}", item, "```", sep="\n")
+}
+
+makechunk_noecho <- function(item) {
+  paste("```{r, echo=FALSE}", item, "```", sep="\n")
+}
+
+#' @importFrom stringr str_split
+makemult <- function(item) {
+  answers <- unlist(str_split(item, ";"))
+  answers <- str_trim(answers)
+  nums <- paste0(seq(length(answers)), ".")
+  paste(nums, answers, collapse="\n")
+}
+
+makemd <- function(unit) UseMethod("makemd")
+
+makemd.default <- function(unit) {
+  stop("No unit class specified!", unit)
+}
+
+makemd.text <- function(unit) {
+  paste(unit[['Output']], 
+        sep="\n\n")
+}
+
+makemd.cmd_question <- function(unit) {
+  paste(unit[['Output']], 
+        makechunk(unit[['CorrectAnswer']]), 
+        sep="\n\n")
+}
+
+makemd.mult_question <- function(unit) {
+  paste(unit[['Output']], 
+        makemult(unit[['AnswerChoices']]), 
+        unit[['CorrectAnswer']], 
+        sep="\n\n")
+}
+
+#' Turn a swirl lesson into a pretty webpage
+#' 
+#' Note that \code{swirl2html()} is an experimental feature. This
+#' it is subject to change based on our own experience and the
+#' feedback we receive from course authors like you.
+#' 
+#' At present, this function only accepts a swirl lesson
+#' formatted in YAML. It detects the lesson you are working on
+#' automatically via \code{getOption('swirlify_lesson_file_path')},
+#' converts it to R Markdown (Rmd), then generates a stylized html
+#' document and opens it in your default browser.
+#' 
+#' The output is formatted to be a readable, standalone tutorial. 
+#' This means that information contained in the swirl lesson such as 
+#' answer tests and hints are excluded from the Rmd/html output.
+#' 
+#' @param destDir destination directory (i.e. where to put the R 
+#' markdown (Rmd) and html output files)
+#' 
+#' @importFrom yaml yaml.load_file
+#' @importFrom rmarkdown render
+#' @export
+swirl2html <- function(destDir) {
+  # Check that a lesson is set
+  lesson_file_check()
+  # Set path to lesson file
+  lessonPath <- getOption('swirlify_lesson_file_path')
+  # Set destination file for Rmd
+  destrmd <- file.path(destDir, "lesson.Rmd")
+  # Load yaml
+  les <- yaml.load_file(lessonPath)
+  # Get and remove meta
+  meta <- unlist(les[1])
+  les <- les[-1]
+  # Write meta to document header
+  cat('---',
+      paste('title:', meta['Lesson']),
+      'output:',
+      '  html_document:',
+      '    theme: spacelab',
+      '---\n',
+      sep="\n", file=destrmd)
+  # Get initLesson.R info and write init chunk w/ no echo
+  initpath <- file.path(dirname(lessonPath), "initLesson.R")
+  initcode <- paste(readLines(initpath, warn=FALSE), collapse="\n")
+  initcode <- paste("suppressPackageStartupMessages(library(swirl))", 
+                    initcode, sep="\n")
+  cat(makechunk_noecho(initcode), "\n\n", file=destrmd, append=TRUE)
+  # Write the rest of the content
+  for(unit in les) {
+    class(unit) <- unit[['Class']]
+    out <- paste(makemd(unit), "\n\n")
+    cat(out, file=destrmd, append=TRUE)
+    invisible()
+  }
+  message("Opening R Markdown file...")
+  file.edit(destrmd)
+  message("Knitting html...")
+  rmarkdown::render(destrmd)
+  # Path to html document
+  desthtml <- file.path(destDir, "lesson.html")
+  message("Opening html document...")
+  browseURL(desthtml)
+}
