7d9d31220a7a9eaede1b12f9128e100793402e46
Nick Carchedi <nick.carchedi@gmail.com>
2014-08-10 11:14:05 -0400

    some much needed updates to swirl2html -- now handles script type questions

--- R/swirl2html.R
+++ R/swirl2html.R
@@ -1,9 +1,11 @@
 makechunk <- function(item) {
-  paste("```{r}", item, "```", sep="\n")
+  out <- c("```{r, strip.white = TRUE}", item, "```")
+  paste0(out, collapse="\n")
 }
 
-makechunk_noecho <- function(item) {
-  paste("```{r, echo=FALSE}", item, "```", sep="\n")
+makechunk_silent <- function(item) {
+  out <- c("```{r, strip.white = TRUE, echo=FALSE, message=FALSE}", item, "```")
+  paste0(out, collapse="\n")
 }
 
 #' @importFrom stringr str_split
@@ -21,54 +23,89 @@ makemd.default <- function(unit) {
 }
 
 makemd.text <- function(unit) {
-  paste(unit[['Output']], 
+  paste(unit[['Output']],
         sep="\n\n")
 }
 
 makemd.cmd_question <- function(unit) {
-  paste(unit[['Output']], 
-        makechunk(unit[['CorrectAnswer']]), 
+  paste(unit[['Output']],
+        makechunk(unit[['CorrectAnswer']]),
         sep="\n\n")
 }
 
 makemd.mult_question <- function(unit) {
-  paste(unit[['Output']], 
-        makemult(unit[['AnswerChoices']]), 
-        unit[['CorrectAnswer']], 
+  paste(unit[['Output']],
+        makemult(unit[['AnswerChoices']]),
+        unit[['CorrectAnswer']],
         sep="\n\n")
 }
 
+makemd.script <- function(unit) {
+  # Get correct script contents
+  script_name <- unit[["Script"]]
+  correct_script_name <- paste0(tools::file_path_sans_ext(script_name),
+                                "-correct.R")
+  path2les <- getOption("swirlify_lesson_dir_path")
+  script_path <- file.path(path2les, "scripts", correct_script_name)
+  script_contents <- readLines(script_path, warn = FALSE)
+  paste(unit[["Output"]],
+        makechunk(script_contents),
+        sep = "\n\n")
+}
+
 #' Turn a swirl lesson into a pretty webpage
-#' 
+#'
 #' Note that \code{swirl2html()} is an experimental feature. This
 #' it is subject to change based on our own experience and the
 #' feedback we receive from course authors like you.
-#' 
+#'
 #' At present, this function only accepts a swirl lesson
 #' formatted in YAML. It detects the lesson you are working on
 #' automatically via \code{getOption('swirlify_lesson_file_path')},
 #' converts it to R Markdown (Rmd), then generates a stylized html
 #' document and opens it in your default browser.
-#' 
-#' The output is formatted to be a readable, standalone tutorial. 
-#' This means that information contained in the swirl lesson such as 
+#'
+#' The output is formatted to be a readable, standalone tutorial.
+#' This means that information contained in the swirl lesson such as
 #' answer tests and hints are excluded from the Rmd/html output.
-#' 
-#' @param destDir destination directory (i.e. where to put the R 
-#' markdown (Rmd) and html output files)
-#' 
+#'
+#' @param dest_dir destination directory (i.e. where to put the rmd
+#' (R Markdown) and html output files). If not set, default is
+#' the lesson directory.
+#' @param keep_rmd should the rmd file be kept after the html is
+#' is produced? Default is \code{TRUE}.
+#'
 #' @importFrom yaml yaml.load_file
 #' @export
-swirl2html <- function(destDir) {
+swirl2html <- function(dest_dir = NULL, keep_rmd = TRUE) {
+  if(!is.logical(keep_rmd)) {
+    stop("Argument keep_rmd must be TRUE or FALSE!")
+  }
   if(!require(rmarkdown)) {
     stop("You must install the rmarkdown package to use this feature!")
   }
   # Check that a lesson is set
   lesson_file_check()
+  # Get course directory and confirm destination dir
+  course_dir <- getOption('swirlify_course_dir_path')
+  # If no dest dir is specified, use the lesson dir
+  if(is.null(dest_dir)) {
+    dest_dir <- getOption("swirlify_lesson_dir_path")
+  }
+  # Check that dest_dir is valid
+  if(!file.exists(dest_dir)) {
+    stop(dest_dir, " does not exist!")
+  }
+  # Expand path
+  dest_dir <- normalizePath(dest_dir)
+  # Install course
+  install_course_directory(course_dir)
   # Set path to lesson file
   lessonPath <- getOption('swirlify_lesson_file_path')
+  # Set rmd file name
+  rmd_filename <- paste0(getOption("swirlify_lesson_dir_name"), ".Rmd")
   # Set destination file for Rmd
-  destrmd <- file.path(destDir, "lesson.Rmd")
+  destrmd <- file.path(dest_dir, rmd_filename)
   # Load yaml
   les <- yaml.load_file(lessonPath)
   # Get and remove meta
@@ -86,10 +123,9 @@ swirl2html <- function(destDir) {
   initpath <- file.path(dirname(lessonPath), "initLesson.R")
   # Get and write initialization code if initLesson.R exists
   if(file.exists(initpath)) {
-    initcode <- paste(readLines(initpath, warn=FALSE), collapse="\n")
-    initcode <- paste("suppressPackageStartupMessages(library(swirl))", 
-                      initcode, sep="\n")
-    cat(makechunk_noecho(initcode), "\n\n", file=destrmd, append=TRUE)
+    initcode <- paste0(readLines(initpath, warn=FALSE), collapse="\n")
+    initcode <- c("suppressPackageStartupMessages(library(swirl))\n", initcode)
+    cat(makechunk_silent(initcode), "\n\n", file=destrmd, append=TRUE)
   }
   # Write the rest of the content
   for(unit in les) {
@@ -103,7 +139,10 @@ swirl2html <- function(destDir) {
   message("Knitting html...")
   rmarkdown::render(destrmd)
   # Path to html document
-  desthtml <- file.path(destDir, "lesson.html")
+  html_filename <- paste0(getOption("swirlify_lesson_dir_name"), ".html")
+  desthtml <- file.path(dest_dir, html_filename)
+  # If keep_rmd is FALSE, remove rmd file
+  if(!keep_rmd) file.remove(destrmd)
   message("Opening html document...")
   browseURL(desthtml)
 }
